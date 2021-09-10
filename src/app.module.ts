40a6605cd847ddc8890e9a4e8009d3569b180b08
PedroManuel <pedromanuelsalas@outlook.com>
2021-09-10 00:24:09 -0500

    mysqlconnect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { ClientesModule } from './clientes/clientes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuariosModule, ClientesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: "us-cdbr-east-04.cleardb.com",
+    port: parseInt("3306"),
+    username: "bf9b789906016e",
+    password: "8ae039f6",
+    database: "heroku_57847ecadbd45d9",
+    autoLoadEntities: true,
+    synchronize: true
+  }),UsuariosModule, ClientesModule],
   controllers: [AppController],
   providers: [AppService],
 })
